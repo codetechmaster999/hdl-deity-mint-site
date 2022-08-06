2f33a3d2c2dd5aab2215bfc890dde95bbd7a607c
codetechmaster999 <sakamotoseiji1125@outlook.com>
2022-08-06 09:02:00 -0500

    build: Got her up and running

---
+++ components/Header/Header.styled.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  min-height: 100px;
+  border-bottom: 3px solid white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.h1`
+  font-size: 4rem;
+  margin: 0;
+`;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as St from './Header.styled';
+
+const Header: React.FC = () => {
+  return (
+    <St.Container>
+      <St.Title>Deity</St.Title>
+    </St.Container>
+  );
+};
+
+export default Header;
